bf1110741fc457881f7da43918e9917b49a6bb3d
ReGuess <7409957+ReGuess@users.noreply.github.com>
2020-07-22 00:07:31 +0000

    Add files via upload

---
+++ get-jssim-url.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# written by ReGuess, 2020 July 21
+set -e
+
+: ${1?"Usage: $0 ARGUMENT"}
+
+prgmhex="$(z80asm -i "$1" -o - | od -v -An -t x1 | sed 's/\s//g' | tr -d '\n')"
+
+printf "http://visual6502.org/JSSim/expert-z80.html?graphics=f"
+printf "&loglevel=-1&logmore=pc,Fetch,a,f,bc,de,hl,ix,iy,sp&a=0000&d="
+python -c "print('"$prgmhex"'.rjust(146, '0'))" # 0 out the test pgrm if needed
