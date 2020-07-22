feccf4421c26707a48994af60de2acad2a49864a
ReGuess <7409957+ReGuess@users.noreply.github.com>
2020-07-22 05:41:57 +0000

    Update get-jssim-url.sh
    miscounted the end of the default test program by one byte

--- get-jssim-url.sh
+++ get-jssim-url.sh
@@ -8,4 +8,4 @@ prgmhex="$(z80asm -i "$1" -o - | od -v -An -t x1 | sed 's/\s//g' | tr -d '\n')"
 
 printf "http://visual6502.org/JSSim/expert-z80.html?graphics=f"
 printf "&loglevel=-1&logmore=pc,Fetch,a,f,bc,de,hl,ix,iy,sp&a=0000&d="
-python -c "print('"$prgmhex"'.ljust(146, '0'))" # 0 out the test pgrm if needed
+python -c "print('"$prgmhex"'.ljust(148, '0'))" # 0 out the test pgrm if needed
